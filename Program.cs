92c385fe526fd0198fbc8111cdcc63dcc40713b6
Katsuki Shuto <stk2k@sazysoft.com>
2022-05-20 15:35:13 +0900

    初回コミット

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MikoVm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // パース処理
+            string src = @"
+a = 3 + 1;
+print a;
+";
+            var tokens = Parser.Parse(src);
+
+            foreach(var tk in tokens)
+            {
+                Console.WriteLine($"{tk}");
+            }
+
+            Console.WriteLine("(何かキーを押すと終了します)");
+            Console.ReadLine();
+        }
+    }
+}
